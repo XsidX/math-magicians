2906da94337017ba19c748b750cc45de2add2165
iamsid <developer.sid@outlook.com>
2022-08-02 15:37:38 +0300

    fix stylelint errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,7 +10,7 @@ export default class Calculator extends Component {
     return (
       <div className="h-full w-full grid grid-rows-6 sm:h-1/2 sm:w-1/2">
         <div className="relative bg-gray-500 row-span-3 sm:row-span-1">
-          <div className="absolute right-4 bottom-2 text-gray-200 md:text-3xl">0</div>
+          <div className="absolute right-4 bottom-2 text-gray-200 text-3xl md:text-2xl">0</div>
         </div>
         <div className="bg-gray-400 grid grid-cols-4 row-span-3 sm:row-span-5 gap-0.125 sm:gap-0.5 ">
           {btnFn.map((btn) => (
