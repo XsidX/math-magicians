ada7e30f516d698de726d6ab7ee7da9a8f752a70
iamsid <developer.sid@outlook.com>
2022-08-13 14:58:41 +0300

    fix layout styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="h-screen text-stone-300 text-2xl text-center px-3 flex justify-center items-center ">
+  <div className="text-stone-400 text-center px-3 flex justify-center items-center ">
     <h1>The only calculator app you will ever need</h1>
   </div>
 );
